508b2b8edd6d599d6f4ca6480127ee9886aba250
sandeepmed <36448171+sandeepmed@users.noreply.github.com>
2018-02-14 15:43:18 +0530

    Add files via upload

---
+++ clsRegionApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILMS
+{
+    class clsRegionApi
+    {
+        public string name { get; set; }
+    }
+
+    class GetRegion
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string NumberOfUsers { get; set; }
+    }
+
+    class clsDepartmentApi
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string NumberOfUsers { get; set; }
+    }
+}
